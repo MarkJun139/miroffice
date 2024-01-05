c9df251731b306e2c5c69d3a918052d543c64674
user <user@cs1h-3L14>
2024-01-05 18:18:27 +0900

    kjs 0105 infoProfile

--- miroffice/src/main/java/com/group4/miroffice/info/InfoController.java
+++ miroffice/src/main/java/com/group4/miroffice/info/InfoController.java
@@ -1,5 +1,40 @@
 package com.group4.miroffice.info;
 
-public class InfoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.group4.miroffice.dto.Users;
+
+@Controller
+@RequestMapping("/main")
+public class InfoController {
+	
+	@Autowired
+	private InfoService service;
+	
+	@GetMapping("/info")
+	public String infoProfile(@AuthenticationPrincipal User user, Model m) {
+		
+		String empname = user.getUsername();
+		
+		Users users = service.Profile(empname);
+		
+		m.addAttribute("user", users);
+		
+		return "info";
+	}
+	
+	@GetMapping("/check")
+	public String infoCheck() {
+		
+		return "check";
+	}
+	
+	
 }
---
+++ miroffice/src/main/java/com/group4/miroffice/info/InfoService.java
@@ -0,0 +1,19 @@
+package com.group4.miroffice.info;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.group4.miroffice.dao.UserDao;
+import com.group4.miroffice.dto.Users;
+
+@Service
+public class InfoService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public Users Profile(String empname) {
+		return userDao.profile(empname);
+	}
+	
+}
