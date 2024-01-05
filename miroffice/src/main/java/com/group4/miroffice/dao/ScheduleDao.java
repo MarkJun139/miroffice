27a685471e7e9cfa4e47475ccd85e1d6abad091c
MinJongHo <alswhdgh12@gmail.com>
2024-01-05 18:10:21 +0900

    240105 일정 추가 기능 임시 구현

--- miroffice/src/main/java/com/group4/miroffice/dao/ScheduleDao.java
+++ miroffice/src/main/java/com/group4/miroffice/dao/ScheduleDao.java
@@ -3,9 +3,12 @@ package com.group4.miroffice.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import com.group4.miroffice.dto.Schedule;
+
 @Mapper
 public interface ScheduleDao {
 
-	
-	
+	@Select("select * from schedule")
+	Schedule mainSchedule(Schedule schedule);
+
 }
