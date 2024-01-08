dc6b8b67e2288924cf057b5cd23cf07e8a6ff893
user <user@cs1h-3L14>
2024-01-08 18:21:08 +0900

    kjs 0108 project

--- miroffice/src/main/java/com/group4/miroffice/info/InfoController.java
+++ miroffice/src/main/java/com/group4/miroffice/info/InfoController.java
@@ -5,10 +5,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.group4.miroffice.dto.UserUpdateDto;
 import com.group4.miroffice.dto.Users;
 
 @Controller
@@ -31,10 +34,37 @@ public class InfoController {
 	}
 	
 	@GetMapping("/check")
-	public String infoCheck() {
+	public String infoCheck(@AuthenticationPrincipal User user, Model m) {
+		
+		String empname = user.getUsername();
+		
+		Users users = service.Profile(empname);
+		
+		m.addAttribute("user", users);
 		
 		return "check";
 	}
+	
+	@PostMapping("/check")
+	public String infoUpdate(@ModelAttribute UserUpdateDto userUpdateDto, @AuthenticationPrincipal User user, BindingResult  result) {
+		
+		if(result.hasErrors()) {
+			return "check";
+		}
+		
+		Users userUpdate = new Users();
+		userUpdate.setEmpPhone(userUpdateDto.getEmpPhone());
+	    userUpdate.setEmpAddress(userUpdateDto.getEmpAddress());
+	    userUpdate.setEmpAddress2(userUpdateDto.getEmpAddress2());
+	    userUpdate.setEmpEmail(userUpdateDto.getEmpEmail());
+	    userUpdate.setEmpPhoto(userUpdateDto.getEmpPhoto());
+	    userUpdate.setEmpPw(userUpdateDto.getEmpPw());
+	    
+	    service.infoUpdate(userUpdate);
+		
+		return "redirect:/";
+	}
+
 	
 	
 }
