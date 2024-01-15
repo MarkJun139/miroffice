27fab2dc9810e28951b6fdc84c0ec73ea1f05c72
MinJongHo <alswhdgh12@gmail.com>
2024-01-15 10:29:02 +0900

    240115 test

--- miroffice/src/main/java/com/group4/miroffice/login/LoginController.java
+++ miroffice/src/main/java/com/group4/miroffice/login/LoginController.java
@@ -3,12 +3,14 @@ package com.group4.miroffice.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class LoginController {
 
 	@GetMapping("/")
 	public String index() {
 		return "login2";
+	}
+
+	void test() {
 	}
 }
