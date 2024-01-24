825028e8edf82c56c1b915f98781b951ecaf3700
user <user@cs1h-3L14>
2024-01-24 17:56:44 +0900

    kjs 0124 project

--- miroffice/src/main/java/com/group4/miroffice/address/AddressService.java
+++ miroffice/src/main/java/com/group4/miroffice/address/AddressService.java
@@ -2,6 +2,7 @@ package com.group4.miroffice.address;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,34 @@ public class AddressService {
 	@Autowired
 	AddressDao addressDao;
 	
-	public List<Address> usersInfo(){
+	public List<Address> getPaginatedAddresses(int page, int pageSize) {
+        int offset = (page - 1) * pageSize;
+        RowBounds rowBounds = new RowBounds(offset, pageSize);
+        return addressDao.usersInfoWithPagination(rowBounds);
+    }
+
+    public int getTotalPages(int pageSize) {
+        long totalItems = addressDao.usersInfo().size();
+        return (int) Math.ceil((double) totalItems / pageSize);
+    }
+    
+    public int SearchtTotalPages(String deptName , int pageSize) {
+        long totalItems = addressDao.searchInfo(deptName).size();
+        return (int) Math.ceil((double) totalItems / pageSize);
+    }
+
+    public List<Address> searchAddressesByDept(String deptName, int page, int pageSize) {
+        int offset = (page - 1) * pageSize;
+        RowBounds rowBounds = new RowBounds(offset, pageSize);
+        return addressDao.searchAddress(deptName, rowBounds);
+    }
+	
+    public List<Address> usersInfo(){
 		return addressDao.usersInfo();
 	}
 	
 	public List<String> deptName(){
 		return addressDao.deptName();
-	}
-	
-	public List<Address> searchAddress(String deptName){
-		return addressDao.searchAddress(deptName);
 	}
 
 }
--- miroffice/src/main/java/com/group4/miroffice/dao/AddressDao.java
+++ miroffice/src/main/java/com/group4/miroffice/dao/AddressDao.java
@@ -4,21 +4,32 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.session.RowBounds;
 
 import com.group4.miroffice.dto.Address;
 
 @Mapper
 public interface AddressDao {
+	
+	@Select("SELECT DISTINCT department.dept_name, employee.* FROM employee "
+            + "INNER JOIN department ON employee.dept_no = department.dept_no ORDER BY department.dept_name")
+    List<Address> usersInfoWithPagination(RowBounds rowBounds);
 
+    @Select("SELECT DISTINCT department.dept_name, employee.* FROM employee "
+            + "INNER JOIN department ON employee.dept_no = department.dept_no "
+            + "WHERE dept_name = #{deptName} ORDER BY department.dept_name")
+    List<Address> searchAddress(String deptName, RowBounds rowBounds);
+	
 	@Select("SELECT DISTINCT department.dept_name, employee.* FROM employee"
 			+ " INNER JOIN department ON employee.dept_no = department.dept_no order by department.dept_name")
 	List<Address> usersInfo();
 	
+	@Select("SELECT DISTINCT department.dept_name, employee.* FROM employee"
+			+ " INNER JOIN department ON employee.dept_no = department.dept_no WHERE dept_name = #{deptName}"
+			+ " order by department.dept_name")
+    List<Address> searchInfo(String deptName);
+	
 	@Select("SELECT dept_name FROM department")
 	List<String> deptName();
 	
-	@Select("SELECT DISTINCT department.dept_name, employee.* FROM employee"
-			+ " INNER JOIN department ON employee.dept_no = department.dept_no"
-			+ " WHERE dept_name = #{deptName} ORDER BY department.dept_name")
-	List<Address> searchAddress(String deptName);
 }
